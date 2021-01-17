d8cfb767102d42e7d544b87c3bf6b78ed0fd7dd7
Leon <leonrubner2809@gmail.com>
2021-01-17 22:56:14 +0100

    routes & controllers setup commit

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { readdirSync } = require('fs');
 require('dotenv').config();
 
 // app
@@ -26,12 +27,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({limit: '2mb'}));
 app.use(cors());
 
-//routes
-app.get('/api', (req, res, next) => {
-    res.json({
-        data: 'you hit the api endpoint.'
-    })
-});
+//routes middleware
+readdirSync('./routes')
+    .map((r) => app.use('/api', require(`./routes/${r}`)));
 
 //port
 const port = process.env.PORT || 8000;
